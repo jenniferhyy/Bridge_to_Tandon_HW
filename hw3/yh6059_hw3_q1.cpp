d5f78a3cec000dd6578045bda4e8ca57fd74b0e0
jenniferhyy <hoyuyanjennifer@gmail.com>
2025-01-21 15:27:44 -0700

    Update yh6059_hw3_q1.cpp

--- hw3/yh6059_hw3_q1.cpp
+++ hw3/yh6059_hw3_q1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iomanip>
 using namespace std; 
+
 
 int main(){
     double item1a, item1b, item2a, item2b, discount, tax, BasePrice, DiscountedPrice, PostTaxPrice; 
@@ -14,27 +16,27 @@ int main(){
     cout << "Enter tax rate, e.g. 5.5 for 5.5% tax: " << endl; 
     cin >> tax; 
     BasePrice = item1a + item2a; 
-    cout << "Base Price: " << BasePrice << endl;
+    cout << "Base Price: " << fixed << setprecision(1) << BasePrice << endl;
 
     if (item1a < item2a) {
         if (ClubCard == 'Y' || ClubCard == 'y') {
             DiscountedPrice = (item1a * 0.5 + item2a) * 0.9;  
-            cout << "Price after discounts: " << DiscountedPrice << endl;
+            cout << "Price after discounts: " << fixed << setprecision(1) << DiscountedPrice << endl;
         } else if (ClubCard == 'N' || ClubCard == 'n') {
             DiscountedPrice = item1a + item2a; 
-            cout << "Price after discounts: " << DiscountedPrice << endl;
+            cout << "Price after discounts: " << fixed << setprecision(1) << DiscountedPrice << endl;
         }
     } else {
         if (ClubCard == 'Y' || ClubCard == 'y') {
             DiscountedPrice = (item2a * 0.5 + item1a) * 0.9;  
-            cout << "Price after discounts: " << DiscountedPrice << endl;
+            cout << "Price after discounts: " << fixed << setprecision(1) << DiscountedPrice << endl;
         } else if (ClubCard == 'N' || ClubCard == 'n') {
             DiscountedPrice = item1a + item2a; 
-            cout << "Price after discounts: " << DiscountedPrice << endl;
+            cout << "Price after discounts: " << fixed << setprecision(1) << DiscountedPrice << endl;
         }
     }
 
     PostTaxPrice = DiscountedPrice * (1 + (tax / 100));
-    cout << "Total price: " << (long double) PostTaxPrice << endl;
+    cout << "Total price: " << fixed << setprecision(5) << PostTaxPrice << endl;
     return 0;
 }
